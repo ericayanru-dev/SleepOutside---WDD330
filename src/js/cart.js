ef44c7508e6bef11031e1f95073d519fc5b30f80
ericayanru-dev <ericayanru@outlook.com>
2025-11-07 22:33:52 +0100

    worked mainly on line 7 to 10 to check and add product to localstorage if it was not already in local storage and not to include it again if it was already there

--- src/js/cart.js
+++ src/js/cart.js
@@ -7,7 +7,7 @@ function renderCartContents() {
 }
 
 function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
+  return `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
     <img
       src="${item.Image}"
@@ -21,8 +21,6 @@ function cartItemTemplate(item) {
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
 </li>`;
-
-  return newItem;
 }
 
 renderCartContents();
--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,21 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const cart = getLocalStorage("so-cart");
+  const existing = cart.find(item => item.Id === product.Id);
+
+  if (existing) {
+    // If found, increase quantity
+    existing.quantity += 1;
+  } else {
+    // If not found, add new product with quantity 1
+    product.quantity = 1;
+    cart.push(product);
+  }
+  setLocalStorage("so-cart", cart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
